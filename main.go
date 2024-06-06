3d8607d4d3993c53098e1a43a7fa7d64c3ea1606
Manav <101001298+imanav10@users.noreply.github.com>
2024-06-06 21:40:01 +0530

    chore: Update movie ID generation to use a larger range

--- main.go
+++ main.go
@@ -57,8 +57,9 @@ func createMovie(w http.ResponseWriter,r *http.Request) {
 	w.Header().Set("Content-Type","application/json")
 	var movie Movie 
 	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
 	movies = append(movies, movie)
+	log.Printf("Movie created: %+v", movie)
 	json.NewEncoder(w).Encode(movie)
 }
 
