56793d8d879c2c7ef34cad3bd8341611350c649f
Manav <101001298+imanav10@users.noreply.github.com>
2024-06-06 19:28:41 +0530

    commit1

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"ecoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	lastname string `json:"lastname"`
+}
