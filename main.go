e15a6bbf60c476e4e6e2ea226976442abc0bd6d6
Manav <101001298+imanav10@users.noreply.github.com>
2024-06-06 20:58:56 +0530

    chore: Add movie CRUD endpoints to main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"ecoding/json"
+	"encoding/json"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -25,14 +25,70 @@ type Director struct{
 
 var movies []Movie
 
-var main() {
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter,r *http.Request) {
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter,r *http.Request) {
+	w.Header().Set("Content-Type","application/json")
+	var movie Movie 
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter,r *http.Request) {
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies= append(movies[:index],movies[index+1:]...)
+			var movie Movie 
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies,movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+
+}
+
+func main() {
 	r := mux.NewRouter()
 
 	movies = append(movies, Movie{ID: "1",Isbn:"42434", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "23132", Title: "Movie two", Director: &Director{Firstname: "jhonny", Lastname: "deph"}})
+
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getmMovie).Method("GET")
-	r.HandleFunc("/movies", createMovie).Method("POST")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 	fmt.Printf("Starting Server at port 8000\n")
